af6cd09e48c6a5378d8f3a1089c8a130c699be05
PyBaker <The Regular Dude from ALX>
2021-07-16 03:59:04 -0700

    Just practicing

--- practice/test2.c
+++ practice/test2.c
@@ -1,11 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-/**
- * main - main function
- * Description: this program does stuff
- * Return 0:Success
- */
 
 int main()
 {
---
+++ test1.c
@@ -0,0 +1,1 @@
+This is just to throw of my bash , silly me right ? 
