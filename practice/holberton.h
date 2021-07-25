4fb100e45b909046f87b258129a2dbbe9a70421a
PyBaker <The Regular Dude from ALX>
2021-07-25 15:26:51 -0700

    Just more practicing :)

--- practice/holberton.h
+++ practice/holberton.h
@@ -1,6 +1,45 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <unistd.h>
 
+void print_alphabet_x10(void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 #endif
--- practice/scrip.c
+++ practice/scrip.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - main function
  * Description: to test write function
@@ -9,21 +9,8 @@
  */
 int main(void)
 {
-	int i, j, k;
-
-	for (i = 0 ; i < 10 ; i++)
-	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			for (k = 0 ; k < 10 ; k++)
-			{if (i------------- == j)
-				continue;
-			putchar('0' + i);
-			putchar('0' + j);
-			putchar(' ');
-			}
-		}
-	}
+	FILE *fd = fopen("~/gat/tinkering_around/practice/hello_world.c", "w");
+	write(fd,1,0);
 	return (0);
 }
 
