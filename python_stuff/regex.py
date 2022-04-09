b2962412a93a7291a7b32e5a8c705b8001b070d3
PyBaker <freddygekko3@gmail.com>
2022-04-09 16:30:24 +0300

    done implimetation of regex

--- python_stuff/regex.py
+++ python_stuff/regex.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import re 
-data = ['Data', 'Usage', '19.7GB', 'Remaining', 'Expires', 'On:', '3', 'Jun', '2022', '2:21', 'PM']
 
 regex= re.compile(r'\d+\.?\d+GB')
 res = list(filter(regex.match, data))
