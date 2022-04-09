745928397a0a15b1b5eb9658385922568dd2f8ff
PyBaker <freddygekko3@gmail.com>
2022-04-09 16:29:25 +0300

    added data set for tests

--- python_stuff/regex.py
+++ python_stuff/regex.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import re 
+data = ['Data', 'Usage', '19.7GB', 'Remaining', 'Expires', 'On:', '3', 'Jun', '2022', '2:21', 'PM']
 
 regex= re.compile(r'\d+\.?\d+GB')
 res = list(filter(regex.match, data))
+#res =re.findall(r'\w',data)
+print(res)
